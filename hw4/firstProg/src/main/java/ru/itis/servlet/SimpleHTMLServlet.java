c34caeebcf353af581214ef1adfc1e3f4c955e89
Adelya <adelasabirzanova@gmail.com>
2020-09-25 20:54:48 +0300

    Improved HW4

--- hw4/firstProg/src/main/java/ru/itis/servlet/SimpleHTMLServlet.java
+++ hw4/firstProg/src/main/java/ru/itis/servlet/SimpleHTMLServlet.java
@@ -8,41 +8,38 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
 
-@WebServlet("/hello")
+@WebServlet("/registration")
 public class SimpleHTMLServlet extends HttpServlet {
     private static final String DB_USERNAME = "postgres";
     private static final String DB_PASSWORD = "adelka0508";
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
 
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("WEB-INF/html/hello.html").forward(req, resp);
+        req.getRequestDispatcher("WEB-INF/html/registration.html").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name_input");
-
+        String firstname = req.getParameter("firstname_input");
+        String secondname = req.getParameter("secondname_input");
+        String email = req.getParameter("email_input");
         try {
-            Connection connection = null;
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            Statement statement = null;
-            statement = connection.createStatement();
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            Class.forName("org.postgresql.Driver");
             //language=SQL
-            String sqlInsertUser = "insert into dannie(name ) " +
-                    "values (?)";
-            PreparedStatement preparedStatement = null;
-            preparedStatement = connection.prepareStatement(sqlInsertUser);
-            preparedStatement.setString(1, name);
+            String sqlInsertUser = "insert into dannie(firstname, secondname, email) " +
+                    "values (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlInsertUser);
+            preparedStatement.setString(1, firstname);
+            preparedStatement.setString(2, secondname);
+            preparedStatement.setString(3, email);
             int affectedRows = preparedStatement.executeUpdate();
             System.out.println(affectedRows);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-
-
-
-
     }
 }
